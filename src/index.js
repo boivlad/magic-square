890b1cd34d885a62ab3404d4cab110b4ba0f8cbb
Vladyslav Boichenko <44983140+boivlad@users.noreply.github.com>
2020-03-24 15:38:11 +0200

    Algorithm (#1)
    * Add algirithm
    
    * delete .idea
    
    * Update README.md
    
    * fix README.md
    
    * add build
    
    * Update README.md
    
    * Resolve conflicts
    
    Co-authored-by: Vladyslav <vladyslav.boichenko@482.solutions>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
- const magicSquare = () => {
-  console.log("Hello");
-  return 1;
-}
-module.exports = magicSquare;
+const combinations = [
+  [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+  [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+  [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+  [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+  [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+  [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+  [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+  [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+];
+
+module.exports.magicSquare = (array) => {
+  let result = 100;
+  for (let i = 0; i < 8; i++) {
+    let total = 0;
+    const currentPattern = combinations[i];
+
+    for (let j = 0; j < 3; j++) {
+      for (let k = 0; k < 3; k++) {
+        if (array[j][k] !== currentPattern[j][k]) {
+          total += (array[j][k] > currentPattern[j][k] ? array[j][k] : currentPattern[j][k]) - (array[j][k] < currentPattern[j][k] ? array[j][k] : currentPattern[j][k]);
+        }
+      }
+    }
+    if (total < result)
+      result = total;
+
+    if (result === 1)
+      return result;
+  }
+  return result;
+};
